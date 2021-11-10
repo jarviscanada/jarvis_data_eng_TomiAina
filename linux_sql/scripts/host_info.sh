8009e18f3a1284a6a06d35fddd11e0b920513ba7
Tomi Aina <aina.oluwatomisin@gmail.com>
2021-11-10 15:23:59 +0000

    monitoring agents

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
 #Setup and validate arguments (again, don't copy comments)
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
-
 #Check # of args
-if [ $# -ne 5]; then
+if [ $# -ne 5 ]; then
   echo 'Please check that all arguments are correct'
   exit 1
 fi
 
 #Save machine statistics in MB and current machine hostname to variables
-vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 lscpu_out=$(lscpu)
 
@@ -21,29 +20,23 @@ lscpu_out=$(lscpu)
 hostname=$(hostname -f)
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | egrep "Model name:" | awk '{print $3}''{print $4}''{print $5}''{print $6}''{print $7}''{print $8}' | xargs)cleac
+cpu_model=$(lscpu  | egrep "Model:" | awk '{print $2}'| xargs)
 cpu_mhz=$(echo "$lscpu_out"  | egrep "CPU MHz:" | awk '{print $3}'|xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "L2 cache:" | awk '{print $3}'|xargs)
-total_mem= $(cat /proc/meminfo | grep MemTotal|awk '{print $2}'|xargs)
-
-#Current time in `2019-11-26 14:40:19` UTC format
-timestamp=  $(vmstat -t|awk '{print $18}'|tail -n1| xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "L2 cache:" | awk '{print $3+0}'|xargs)
+timestamp=$(date +%Y-%m-%d\ %H\:%M\:%S\.%3N)
+total_mem=$(cat /proc/meminfo | head -1 |awk '{print $2}')
 
 #Subquery to find matching id in host_info table
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
-cpu_number= "(SELECT cpu_number FROM host_info WHERE cpu_number= '$cpu_number')";
-cpu_architecture= "(SELECT cpu_architecture FROM host_info WHERE cpu_number = '$cpu_architecture')";
-cpu_model= "(SELECT cpu_model FROM host_info WHERE cpu_model= '$cpu_model')";
-cpu_mhz= "(SELECT cpu_mhz FROM host_info WHERE cpu_mhz = '$cpu_mhz')";
-l2_cache= "(SELECT l2_cache FROM host_info WHERE l2_cache = '$l2_cache')";
-total_mem= "(SELECT total_mem FROM host_info WHERE total_mem = '$total_mem')";
 
 #PSQL command: Inserts server usage data into host_usage table
 #Note: be careful with double and single quotes
-insert_stmt="INSERT INTO host_usage(timestamp, cpu_number,cpu_model,cpu_mhz,l2_cache,total_mem) VALUES('$timestamp', '$cpu_number','$cpu_model','$cpu_mhz','$l2_cache','$total_mem')"
-
+set_insert_data=$(cat <<-END
+INSERT into host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, "timestamp", total_mem)  values  ( '${hostname}', ${cpu_number}, '${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${l2_cache}, '${timestamp}', ${total_mem} );
+END
+)
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
 #Insert date into a database
-psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+psql -h localhost -p 5432 -U postgres -d host_agent -c "$set_insert_data"
 exit $?
